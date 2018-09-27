462e20e8d41688b9769fe62593fa5c497a2b2b22
KosMax01 <shitcodernumber1@gmail.com>
2018-09-27 16:51:28 +0600

    что то не то с регистрацией

--- Lifestyle/Controllers/HomeController.cs
+++ Lifestyle/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Lifestyle.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace Lifestyle.Controllers
 {
@@ -12,7 +14,35 @@ namespace Lifestyle.Controllers
         {
             return View();
         }
-               
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(Authorization model)
+        {
+            if (ModelState.IsValid)
+            {
+                // поиск пользователя в бд
+                User user = null;
+                using (UserContext db = new UserContext())
+                {
+                    user = db.Users.FirstOrDefault(u => u.Email == model.Login && u.Password == model.Password);
+
+                }
+                if (user != null)
+                {
+                    FormsAuthentication.SetAuthCookie(model.Login, true);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Пользователя с таким логином и паролем нет");
+                }
+            }
+
+            return View(model);
+        }
+
 
         public ActionResult Help()
         {
